45b960486fff03fc18aead9c9e37b22219cdf413
Alejandro Rivera <alejandrodrivera239@gmail.com>
2026-04-13 21:43:04 -0500

    Initial project setup

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Interactive Portfolio Analytics App", layout="wide")
+
+st.title("Interactive Portfolio Analytics App")
+st.write("Project setup is working.")
